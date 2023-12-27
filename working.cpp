bfe907e562d458a37b09ab7966b88c5affecbd9a
Dagmawi Beyene <dagmawibete@gmail.com>
2023-12-27 21:56:21 +0200

    check #

--- working.cpp
+++ working.cpp
@@ -174,6 +174,14 @@ void stopOrResetIfNeeded()
     }
 }
 
+void checkForImmediateStop() {
+    char key = keypad.getKey();
+    if (key == '#') {
+        stopEverything();
+    }
+}
+
+
 void loop()
 {
     if (!nValueSet)
@@ -184,6 +192,8 @@ void loop()
     }
     else if (nValueSet)
     {
+        // Check the immediate stop or reset commands here
+        checkForImmediateStop();
 
         // Check if Limit Switch 1 is pressed to start the motor sequence
         if (digitalRead(limitswitch1) == LOW)
@@ -199,8 +209,9 @@ void loop()
         }
     }
 
-    // All main logic is handled within sub-functions
+     // All main logic is handled within sub-functions
 }
+
 
 void motorReverseUntilLimitSwitch2()
 {
@@ -387,19 +398,14 @@ void startMotorSequence()
             lcd.print("Motor is ON ");
             lcd.print(loopCount + 1);
 
-            while (!isMotorRunning)
-            {
-                delay(10);             // Wait for a short period to prevent tightly locked loop
-                stopOrResetIfNeeded(); // <--- HERE
-            }
+            checkForImmediateStop(); // <--- HERE
+
 
             // Forward loop operation
             delay(motorDelayTime);
             while (!isMotorRunning)
-            {
-                delay(10);             // Wait for a short period to prevent tightly locked loop
-                stopOrResetIfNeeded(); // <--- HERE
-            }
+            checkForImmediateStop(); // <--- HERE
+
             digitalWrite(motorPin1, HIGH);
             digitalWrite(motorPin2, LOW);
             delay(3000);
@@ -460,9 +466,7 @@ void stopEverything()
     motorActive = false; // Update motorActive flag to stop the loop operation
 
     lcd.clear();
-    lcd.print("All stopped!"); // Notify the user
-    delay(1000);               // Wait for 1 second
-    resetArduino();            // Resets the system to initial state
+    lcd.print("All stopped!"); // Notify the user// Resets the system to initial state
     isMotorRunning = false;
 }
 
